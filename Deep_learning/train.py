fcf418eb644b218a730a8aa708794722c9f045ec
ishan silwal <ishansilwal3@gmail.com>
2025-12-23 17:54:39 +0545

    Unet segmentation and yolo

--- Deep_learning/train.py
+++ Deep_learning/train.py
@@ -1,4 +1,4 @@
-from ultralytics import YOLO
+from ultralytics import YOLO, SAM
 
 if __name__ == '__main__':  
     # model = YOLO("yolo11n.pt") 
@@ -11,7 +11,19 @@ if __name__ == '__main__':
     #     name="my_custom_model" 
     # )
 
-    model = YOLO("yolov5n.pt")
+    # model = YOLO("yolov5n.pt")
+    # model.info()
+
+    # results = model.train(data="coco8.yaml", project = r"runs/custom_training", epochs=5, imgsz=640)
+
+     # Load a model
+    model = SAM("sam_b.pt")
     model.info()
 
-    results = model.train(data="coco8.yaml", project = r"runs/custom_training", epochs=5, imgsz=640)
+    print("Starting inference...")
+    results = model("D:\\CV practice\\Deep_learning\\dataset\\Train\\images\\_image_1763623303_aug_20251212-164529053914.jpg")
+    print("Inference complete.")
+
+    # Save each result image
+    for i, res in enumerate(results):
+        res.save(filename=f"output_{i}.jpg")
